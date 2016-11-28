a0563fb542f55c7a4802b383ca5361472604a797
Tyler <tsm33@hw.ac.uk>
2016-11-28 00:20:42 +0000

    Added comments

--- runecj.sh
+++ runecj.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-
+# Original script provided by Dr. Michael Lones
 # Command line arguments: number-of-repeats parameters-file
-
+# Calculates the time in milliseconds for line 9 to run
+# Probably reasonably inaccurate in calculating the "true" run time
 for i in `seq 1 $1`; do
 	echo "Run "$i
 	ts=$(date +%s%N)
